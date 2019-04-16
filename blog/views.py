1d7f7c0528126d7af2db11a0ab9fa2e3eb26cfec
Wasth <riasta@yandex.ru>
2019-04-16 17:42:52 +0300

    First test view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from blog.models import Post
+
+
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/index.html'
