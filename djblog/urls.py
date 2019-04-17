274cc6b13d0418e192d08864b62790c20a4bda0d
Wasth <riasta@yandex.ru>
2019-04-17 11:33:51 +0300

    url rule for index and registered model(post, category) to admin

--- djblog/urls.py
+++ djblog/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import blog
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('', blog.views.PostList.as_view()),
     # path('', include('blog.urls'))
 ]
