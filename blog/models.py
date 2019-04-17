ae133d7549e7b1e0c4b82b979dbaa07cac55115d
Wasth <riasta@yandex.ru>
2019-04-17 11:35:51 +0300

    __str__ methods in models

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=32)
 
+    def __str__(self):
+        return name
 
 class Post(models.Model):
     title = models.CharField(max_length=32)
@@ -13,10 +15,16 @@ class Post(models.Model):
     image = models.ImageField(upload_to='blog/static/blog/images')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=32)
     post = models.ManyToManyField(Post)
+
+    def __str__(self):
+        return self.name
 
 
 class Comment(models.Model):
@@ -24,6 +32,8 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.username + ' - '+self.text[:10]
 
 class Profile(models.Model):
     avatar = models.ImageField(upload_to='blog/static/blog/images')
