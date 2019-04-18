8be1e6a62dad0a3c13150fcc9185f3e32390f050
Wasth <riasta@yandex.ru>
2019-04-18 11:25:21 +0300

    Tag relationship with Post changed

--- blog/models.py
+++ blog/models.py
@@ -8,23 +8,24 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=32)
     anons = models.CharField(max_length=100)
     text = models.TextField()
     image = models.ImageField(upload_to='blog')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    tag = models.ManyToManyField(Tag, null=True)
 
     def __str__(self):
         return self.title
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=32)
-    post = models.ManyToManyField(Post)
-
-    def __str__(self):
-        return self.name
 
 
 class Comment(models.Model):
