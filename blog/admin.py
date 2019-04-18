34598ef41ed11956247b023f0b3409b38c653407
Wasth <riasta@yandex.ru>
2019-04-18 09:54:28 +0300

    Registered models to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from blog.models import Post, Category
+from blog.models import Post, Category, Tag, Comment
 
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Comment)
