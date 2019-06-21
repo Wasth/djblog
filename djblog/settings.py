4f29aa349515760d0f2b9b5e96de5acc039b5522
Wasth <riasta@yandex.ru>
2019-06-21 11:44:53 +0300

    Redirecting url after login/logout

--- djblog/settings.py
+++ djblog/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
